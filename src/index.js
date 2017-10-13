334a8eaf4969242f5cb09d98991614f021a042ea
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-10-12 22:59:20 -0700

    update

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import rootReducer from './reducers';
 import { addCharacterById } from './actions';
 
 const store = createStore(rootReducer);
-console.log('store.getState()', store.getState());
-store.subscribe(() => console.log('store', store.getState()));
-store.dispatch(addCharacterById(2));
+console.log('store.getState()', store.getState()); // return current state
+store.subscribe(() => console.log('store', store.getState()));   // Listen
+store.dispatch(addCharacterById(2));    // trigger
 
 ReactDOM.render(
   <Provider store={store}>
